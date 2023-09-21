301a45271c643e209183974ad7dc3ca0477a13dd
sneha kumari <snehakumari64445@gmail.com>
2023-09-21 19:54:52 +0530

    splash screen yo

--- THREATSIFT/ios/THREATSIFT/AppDelegate.mm
+++ THREATSIFT/ios/THREATSIFT/AppDelegate.mm
@@ -1,7 +1,7 @@
 #import "AppDelegate.h"
 
 #import <React/RCTBundleURLProvider.h>
-
+#import "RNSplashScreen.h" 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
@@ -9,9 +9,11 @@
   self.moduleName = @"THREATSIFT";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
+  bool finish=[super application:application didFinishLaunchingWithOptions:launchOptions]
+  [RNSplashScreen show]; 
   self.initialProps = @{};
 
-  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+  return finish;
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
